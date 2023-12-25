914ef35f2c7589f6233d51011afa9816ef72264f
Sumitkumar104 <parjapatsumit104@gmail.com>
2023-12-25 12:19:57 +0530

    Add logic of "create" in createTodo.js

---
+++ controllers/createTodo.js
@@ -0,0 +1,29 @@
+// Import the Todo model from the "../models/Todo" file
+const Todo = require("../models/Todo");
+
+// Define an asynchronous function named "createtodo"
+exports.createtodo = async (req, res) => {
+  try {
+    // Extract the "title" and "description" from the request's body
+    const { title, description } = req.body;
+
+    // Create a new Todo object and insert it into the database using the Todo model
+    const response = await Todo.create({ title, description });
+
+    // Respond with a success status, the created data, and a success message
+    res.status(200).json({
+      success: true,
+      data: response,
+      message: "Entry created successfully",
+    });
+  } catch (err) {
+    // Handle errors by logging them and responding with an error status and message
+    console.log(err);
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      data: "Internal server error",
+      message: err.message,
+    });
+  }
+};
--- models/Todo.js
+++ models/Todo.js
@@ -1,2 +1,30 @@
 const mongoose=require('mongoose');
-const todoscheme
+const todoscheme =new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        maxLength:50,
+
+    },
+    description:{
+        type:String,
+        required:true,
+        maxLength:100,
+    },
+    createdat:{
+        type:Date,
+        required:true,
+        default:Date.now(),
+
+    },
+    updatedat:{
+        type:Date,
+        required:true,
+        default:Date.now(),
+    }
+
+})
+
+// Create and export a Mongoose model named "Todo" based on the todoscheme schema. The mongoose.model() function is used to create a model. It takes two arguments: the model name ("Todo") and the schema (todoscheme). This model can then be used to interact with the MongoDB collection corresponding to "Todo" documents.
+module.exports=mongoose.model("Todo",todoscheme);
+// further it is used by controller .
